d292b223207b08da632172997e9ff467a967b270
Barbara Blank <barbaracbgaribaldi@gmail.com>
2019-11-26 22:48:41 -0300

    Config docker postgres

--- Backend/Api/src/main/java/com/repeteco/teste/TesteApplication.java
+++ Backend/Api/src/main/java/com/repeteco/teste/TesteApplication.java
@@ -1,9 +1,7 @@
 package com.repeteco.teste;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
