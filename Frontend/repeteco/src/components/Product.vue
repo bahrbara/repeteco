0f6295de37917ddd2d590a13cdcc500b7e175eb5
Barbara Blank <barbaracbgaribaldi@gmail.com>
2019-12-10 17:47:47 -0300

    Atualizando icones

--- Frontend/repeteco/src/components/Product.vue
+++ Frontend/repeteco/src/components/Product.vue
@@ -4,8 +4,6 @@
       <div class="col-lg-3 col-md-4 col-sm-6 col-12">
         <div class="product__thumb">
           <a v-if="this.data.imagem1" class="first__img" href="#"><img v-bind:src="'data:image/png;base64,' + this.data.imagem1" alt="product image"></a>
-          <!-- <a class="first__img" href="#"><img src="../assets/books/1.jpg" alt="product image"></a> -->
-          <!-- <a class="second__img animation1" href="#"><img src="../assets/books/2.jpg" alt="product image"></a> -->
           <div class="hot__box">
             <span class="hot-label">{{this.data.conservacao}}</span>
           </div>
@@ -18,7 +16,7 @@
           <div class="action">
             <div class="actions_inner">
               <ul class="add_to_links">
-                <li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+                <li><a class="compare" href="#"><i class="bi bi-pencil"></i></a></li>
                 <li><a data-toggle="modal" @click="apagar()" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-cross"></i></a></li>
               </ul>
             </div>
